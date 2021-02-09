36fa1b770592337c0f572013ad2bb28f5fcebe77
fayzan ali <fayzanali97@gmail.com>
2021-02-09 20:13:45 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,14 @@
+const userList = [
+    {
+        id: 0,
+        name: 'Raj',
+        email: 'raj12@gmail.com'
+    },
+    {
+        id: 1,
+        name: 'abhishek',
+        email: 'abhishek05@gmail.com'
+    }
+];
+
+export default userList;
